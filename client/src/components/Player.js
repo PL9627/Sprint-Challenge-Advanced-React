dc189fc8fa2fa9dd6cae0680f2cf64144f97908c
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-17 16:30:38 -0700

    finished with Player.js

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Players = ({player}) => {
+const Player = ({player}) => {
     return (
         <div>
             <div>
-                
+                <h1>Name of Soccer Player:</h1>
+                <h2>{player.name}</h2>
+                <h1>Women's World Cup Team:</h1>
+                <h2>{player.country}</h2>
             </div>
         </div>
     )
 }
 
-export default Players;
+export default Player;
