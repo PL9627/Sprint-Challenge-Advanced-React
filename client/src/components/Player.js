1d6d76eaf9005f87e1c05f512e516248f25fd049
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-17 16:27:42 -0700

    started Player.js for player data

---
+++ client/src/components/Player.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Players = ({player}) => {
+    return (
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Players;
