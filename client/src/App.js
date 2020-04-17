e7a3bd96a10737f5a67f630ea702d13ee6f51214
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-17 16:21:08 -0700

    switched to react class component for App and started on componentDidMount

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    console.log('constructor runs')
+
+    super();
+
+    this.state = {
+      player: []
+    }
+  };
+
+  componentDidMount() {
+    console.log('componentDidMount');
+
+    axios.get('http://localhost:5000/api/players')
+    .then(players => {
+      console.log('players:', players);
+    })
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
+  }
 }
 
 export default App;
